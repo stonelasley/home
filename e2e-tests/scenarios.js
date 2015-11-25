b36de5362747f87a1670afab767844e9b4d7ea1a
Stone C. Lasley <stonelasley@gmail.com>
2015-11-24 23:18:04 -0700

    Disallow invalid form submission

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,6 +2,12 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
+var hasClass = function (element, cls) {
+  return element.getAttribute('class').then(function (classes) {
+    return classes.split(' ').indexOf(cls) !== -1;
+  });
+};
+
 describe('my app', function() {
 
 
@@ -27,6 +33,12 @@ describe('my app', function() {
 
 
   describe('contact', function() {
+    var name = element(by.model('contact.name'));
+    var phone = element(by.model('contact.phone'));
+    var email = element(by.model('contact.email'));
+    var message = element(by.model('contact.message'));
+    var submit = element(by.buttonText("Submit"));
+    var confirmation = element(by.id('contact-confirm'));
 
     beforeEach(function() {
       browser.get('#/contact');
@@ -36,6 +48,29 @@ describe('my app', function() {
     it('should render contact when user navigates to /contact', function() {
       expect(element.all(by.css('[ui-view] h1')).first().getText()).
         toMatch('Contact Me');
+    });
+
+    it('should allow submission', function() {
+      name.sendKeys('name');
+      phone.sendKeys('123456');
+      email.sendKeys('email@google.com');
+      message.sendKeys('message text');
+      submit.click();
+
+      expect(confirmation.getText()).
+        toMatch('Thank you');
+    });
+
+    it('should disallow submission for invalid email', function() {
+      name.sendKeys('name');
+      phone.sendKeys('123456');
+      email.sendKeys('invalidemail.com');
+      message.sendKeys('message text');
+      submit.click();
+
+      expect(hasClass(email, 'ng-invalid')).toBe(true);
+      expect(submit.isEnabled()).toBe(false);
+      browser.pause();
     });
 
   });
